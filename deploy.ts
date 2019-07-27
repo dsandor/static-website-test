650ed0af67a1287bf9d60441c0b4348c1c4cb9ba
dsandor <david.sandor@gmail.com>
2019-07-27 08:10:20 -0400

    Initial

---
+++ deploy.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import cdk = require('@aws-cdk/core');
+import { StaticWebsiteStack, IStaticWebsiteProps } from 'aws-cdk-static-website'; //'./StaticWebsiteStack';
+
+export class MyWebStack extends StaticWebsiteStack {
+  constructor(scope: cdk.App, id: string ) {
+    const props: IStaticWebsiteProps = {
+      websiteDistPath: './dist',
+      deploymentVersion: '1.0.0',
+      resourcePrefix: 'my-web-stack',
+      indexDocument: 'index.html',
+    };
+
+    super(scope, id, props);
+  }
+}
+
+const app = new cdk.App();
+new MyWebStack(app, 'MyWebStack');
